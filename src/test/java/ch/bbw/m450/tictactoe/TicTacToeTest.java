cd974e289f934025e63aecd63cf44fde1b587d47
okurshike <mariia.fedorchenko@lernende.bbw.ch>
2026-09-06 12:22:35 +0000

    implemented Parameterized Tests and created Dev Container

--- src/test/java/ch/bbw/m450/tictactoe/TicTacToeTest.java
+++ src/test/java/ch/bbw/m450/tictactoe/TicTacToeTest.java
@@ -7,26 +7,25 @@ import ch.bbw.m450.tictactoe.TicTacToePlayer.Stone;
 import ch.bbw.m450.tictactoe.players.GreedyPlayer;
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 public class TicTacToeTest {
-	@Test
-	void dummy() {
-		assertThat(1 + 1).isEqualTo(2);
-	}
-
     @Test
-    void isWin_detectsRowWin() {
-        var board = rowWinBoard();
+    void dummy() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
 
-		assertThat(TicTacToeMain.isWin(board, Stone.CROSS)).isTrue();
-	}
+    @ParameterizedTest
+    // test daten aus methode "winningboards" holen
+    @MethodSource("winningBoards")
+    void isWin_returnsTrueForWinningBoards(Stone[] board, Stone player) {
+        assertThat(TicTacToeMain.isWin(board, player)).isTrue();
+    }
 
-    @Test
-    void isWin_detectsDiagonalWin() {
-        var board = diagonalWinBoard();
-
-		assertThat(TicTacToeMain.isWin(board, Stone.CROSS)).isTrue();
-	}
-
+    // ausfs gleiche Feld Cross und Circle gleichzeitig
     @Test
     void play_rejectsUsingSamePlayerForCrossAndCircle() {
         var player = greedyPlayer();
@@ -35,15 +34,15 @@ public class TicTacToeTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void isWin_returnsFalse_whenBoardHasNoWinner() {
-        var board = boardWithoutWinner();
-
-        assertThat(TicTacToeMain.isWin(board, Stone.CROSS)).isFalse();
-        assertThat(TicTacToeMain.isWin(board, Stone.CIRCLE)).isFalse();
+    @ParameterizedTest
+    // test daten aus methode "nonwinningboards" holen
+    @MethodSource("nonWinningBoards")
+    void isWin_returnsFalseForNonWinningBoards(Stone[] board, Stone player) {
+        assertThat(TicTacToeMain.isWin(board, player)).isFalse();
     }
 
     @Test
+    // 2 greedyplayer spielen(X0-O1-X2-O3-X4-O5-X6...)
     void play_twoGreedyPlayers_resultsInCrossWinning() {
         var crossPlayer = greedyPlayer();
         var circlePlayer = greedyPlayer();
@@ -53,37 +52,61 @@ public class TicTacToeTest {
         assertThat(winner).isEqualTo(Stone.CROSS);
     }
 
-    
     // Helpers
-
     private GreedyPlayer greedyPlayer() {
         return new GreedyPlayer();
     }
 
-
     // Fixtures
+    private static Stream<Object[]> winningBoards() {
+        return Stream.of(
+                new Object[] {
+                        new Stone[] {
+                                Stone.CROSS, Stone.CROSS, Stone.CROSS,
+                                null, Stone.CIRCLE, null,
+                                Stone.CIRCLE, null, null
+                        },
+                        Stone.CROSS
+                },
 
-    private Stone[] rowWinBoard() {
-        return new Stone[]{
-                null, Stone.CIRCLE, null,
-                Stone.CROSS, Stone.CROSS, Stone.CROSS,
-                Stone.CIRCLE, null, null
-        };
-    }
+                new Object[] {
+                        new Stone[] {
+                                Stone.CROSS, null, Stone.CIRCLE,
+                                null, Stone.CROSS, null,
+                                Stone.CIRCLE, null, Stone.CROSS
+                        },
+                        Stone.CROSS
+                },
 
-    private Stone[] diagonalWinBoard() {
-        return new Stone[]{
-                Stone.CROSS, null, Stone.CIRCLE,
-                null, Stone.CROSS, null,
-                Stone.CIRCLE, null, Stone.CROSS
-        };
-    }
+                new Object[] {
+                        new Stone[] {
+                                Stone.CIRCLE, Stone.CROSS, null,
+                                Stone.CIRCLE, null, Stone.CROSS,
+                                Stone.CIRCLE, null, null
+                        },
+                        Stone.CIRCLE
+                }
+        );
+    };
 
-    private Stone[] boardWithoutWinner() {
-        return new Stone[]{
-                Stone.CROSS, Stone.CIRCLE, Stone.CROSS,
+    private static Stream<Object[]> nonWinningBoards() {
+        return Stream.of(
+            new Object[] {
+                new Stone[] {
+                    Stone.CROSS, Stone.CIRCLE, Stone.CROSS,
+                    Stone.CROSS, Stone.CIRCLE, Stone.CIRCLE,
+                    Stone.CIRCLE, Stone.CROSS, Stone.CROSS
+                },
+                Stone.CROSS
+            },
+            new Object[] {
+                new Stone[] {
+                    Stone.CROSS, Stone.CIRCLE, Stone.CROSS,
                 Stone.CROSS, Stone.CIRCLE, Stone.CIRCLE,
                 Stone.CIRCLE, Stone.CROSS, Stone.CROSS
-        };
-    }
+                },
+                Stone.CIRCLE
+            }
+        );
+    };
 }
